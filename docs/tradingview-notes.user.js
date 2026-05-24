b1e8cd4febe60e81c888bcfe0983e2d7ea94d6fb
Patiphan <patiphan.injob@gmail.com>
2026-05-24 21:06:39 +0700

    Keep TradingView userscript as no-op

--- docs/tradingview-notes.user.js
+++ docs/tradingview-notes.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         🍺 Beer Vanon TradingView Integration Disabled
 // @namespace    https://patiphaninjob-lang.github.io/beer-vanon-agents/
-// @version      2.3.0
-// @description  ปิดการรันบน TradingView แล้ว เพื่อไม่เด้ง popup/script และให้ทำการบ้านใน Beer Top 100 อย่างเดียว
+// @version      2.4.0
+// @description  ปิด Pine/marker automation บน TradingView แล้ว ให้ทำการบ้านใน Beer Top 100 อย่างเดียว
 // @author       Patiphan
-// @match        https://patiphaninjob-lang.github.io/beer-vanon-agents/__disabled_tradingview_integration__
+// @match        https://*.tradingview.com/*
 // @grant        none
 // @run-at       document-end
 // @updateURL    https://patiphaninjob-lang.github.io/beer-vanon-agents/tradingview-notes.user.js
