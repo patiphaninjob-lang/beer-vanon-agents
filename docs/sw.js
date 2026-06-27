841dbe083d95dc79055cc3f0a60b0e1c9b540396
Patiphan <patiphan.injob@gmail.com>
2026-06-27 22:32:23 +0700

    📈 feat: Implement Y-coordinate candlestick hit testing (v2.6.0)

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'beer-top100-v20260627-candlestick-3d-glow-v16';
+const CACHE_NAME = 'beer-top100-v20260627-candlestick-3d-glow-v17';
 const APP_SHELL = [
   './',
   './index.html',
