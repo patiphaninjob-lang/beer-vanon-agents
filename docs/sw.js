31e9731f6afbc2bed0ba6e1b5a20a0e89115c6c7
Patiphan <patiphan.injob@gmail.com>
2026-06-27 21:02:01 +0700

    📈 feat: Upgrade to 3D glowing candlesticks & iOS springy Glassmorphism popups (v2.5.0)

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'beer-top100-v20260627-history-journal-sync-3';
+const CACHE_NAME = 'beer-top100-v20260627-candlestick-3d-glow-v1';
 const APP_SHELL = [
   './',
   './index.html',
