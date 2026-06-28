30ff4edc58f079c7019068d0d5963a41c9747f29
Patiphan <patiphan.injob@gmail.com>
2026-06-28 19:34:51 +0700

    style: Enable free tooltip dragging on mobile in Home and History by removing bottom-pinning media queries (v3.5.2)

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'beer-top100-v20260628-candlestick-3d-glow-v43';
+const CACHE_NAME = 'beer-top100-v20260628-candlestick-3d-glow-v44';
 const APP_SHELL = [
   './',
   './index.html',
