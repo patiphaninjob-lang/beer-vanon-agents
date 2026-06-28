10e6005e1d076faee6db7ec0bcfe6cc1a60a3be6
Patiphan <patiphan.injob@gmail.com>
2026-06-28 08:23:22 +0700

    fix: Draw connector line when clicking list triggers/note buttons and add SVG width/height attributes for browser/webview scaling (v2.9.0)

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'beer-top100-v20260628-candlestick-3d-glow-v22';
+const CACHE_NAME = 'beer-top100-v20260628-candlestick-3d-glow-v23';
 const APP_SHELL = [
   './',
   './index.html',
