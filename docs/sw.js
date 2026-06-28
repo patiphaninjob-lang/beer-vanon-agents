2c50f84ca7df54ccdbab12dfce75aa5fe8a25f45
Patiphan <patiphan.injob@gmail.com>
2026-06-28 20:25:47 +0700

    feat(pwa): add None option to outcome and allow deselecting setup/outcome chips (v3.6.1)

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'beer-top100-v20260628-candlestick-3d-glow-v46';
+const CACHE_NAME = 'beer-top100-v20260628-candlestick-3d-glow-v47';
 const APP_SHELL = [
   './',
   './index.html',
