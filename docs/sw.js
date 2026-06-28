374e30e0bb294b65ef4646c2d8b359b6f6a3d2ef
Patiphan <patiphan.injob@gmail.com>
2026-06-28 13:54:33 +0700

    feat: Lock tooltip position in place when navigating left/right to prevent jitter (v3.2.1)

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'beer-top100-v20260628-candlestick-3d-glow-v29';
+const CACHE_NAME = 'beer-top100-v20260628-candlestick-3d-glow-v30';
 const APP_SHELL = [
   './',
   './index.html',
