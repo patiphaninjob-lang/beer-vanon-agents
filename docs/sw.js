d0b919bea87b6360b85394fa24bd7dd7403ef827
Patiphan <patiphan.injob@gmail.com>
2026-06-28 20:38:59 +0700

    feat(journal): allow deselecting emotion and intent chips (v3.6.2)

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'beer-top100-v20260628-candlestick-3d-glow-v47';
+const CACHE_NAME = 'beer-top100-v20260628-candlestick-3d-glow-v48';
 const APP_SHELL = [
   './',
   './index.html',
