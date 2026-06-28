c2baa3416e076c6bbefcf4059bde20e2522fade0
Patiphan <patiphan.injob@gmail.com>
2026-06-28 07:52:49 +0700

    feat: Upgrade tooltip to dark glassmorphism, add dynamic SVG connector line, and support drag-and-drop (v2.7.0)

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'beer-top100-v20260627-candlestick-3d-glow-v17';
+const CACHE_NAME = 'beer-top100-v20260628-candlestick-3d-glow-v18';
 const APP_SHELL = [
   './',
   './index.html',
