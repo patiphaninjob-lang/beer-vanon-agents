223ce4ae88ec64a686573b4ee1c9d1a48e388eb0
Patiphan <patiphan.injob@gmail.com>
2026-06-28 14:32:02 +0700

    style: Strip 'Beer มองว่า:' prefix dynamically from analysis output in cards and tooltips (v3.2.8)

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'beer-top100-v20260628-candlestick-3d-glow-v36';
+const CACHE_NAME = 'beer-top100-v20260628-candlestick-3d-glow-v37';
 const APP_SHELL = [
   './',
   './index.html',
