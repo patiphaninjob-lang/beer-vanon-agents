08c82a2374e6eab30e0ee2e8ea1d5764bb388c26
Patiphan <patiphan.injob@gmail.com>
2026-06-28 13:49:36 +0700

    feat: Add interactive navigation arrows to Tooltip header across all views (v3.2.0)

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'beer-top100-v20260628-candlestick-3d-glow-v28';
+const CACHE_NAME = 'beer-top100-v20260628-candlestick-3d-glow-v29';
 const APP_SHELL = [
   './',
   './index.html',
