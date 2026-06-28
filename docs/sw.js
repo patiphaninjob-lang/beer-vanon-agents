a8f41f9dd1788b54a81c8a93d6bc0096a1d0f59d
Patiphan <patiphan.injob@gmail.com>
2026-06-28 19:28:02 +0700

    fix: Resolve mobile touch blocking bug on tooltip navigation arrows and link buttons by ignoring e.preventDefault on button/link touchstart (v3.5.1)

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'beer-top100-v20260628-candlestick-3d-glow-v42';
+const CACHE_NAME = 'beer-top100-v20260628-candlestick-3d-glow-v43';
 const APP_SHELL = [
   './',
   './index.html',
