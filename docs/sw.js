e812ad9d2d0b21f6e2909d05206e9d03e6fa0e42
Patiphan <patiphan.injob@gmail.com>
2026-06-28 07:59:26 +0700

    feat: Port premium draggable glassmorphic tooltips and SVG connector lines to History and Journal pages (v2.7.5)

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'beer-top100-v20260628-candlestick-3d-glow-v18';
+const CACHE_NAME = 'beer-top100-v20260628-candlestick-3d-glow-v19';
 const APP_SHELL = [
   './',
   './index.html',
