025f5659b57fe26720c44b6a31605cdbae43c712
Patiphan <patiphan.injob@gmail.com>
2026-06-29 20:58:42 +0700

    feat(tooltip): default tooltip positioning below-left of marker (v3.6.4)

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'beer-top100-v20260628-candlestick-3d-glow-v49';
+const CACHE_NAME = 'beer-top100-v20260628-candlestick-3d-glow-v50';
 const APP_SHELL = [
   './',
   './index.html',
