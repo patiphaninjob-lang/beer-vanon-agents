5f7f189c722c2ab907b96caf90db899f732d973e
Patiphan <patiphan.injob@gmail.com>
2026-07-04 17:12:44 +0700

    feat(homepage): render homework completion timestamp on top of each stock card in Home Page grid, bump version to v3.9.3 and sw.js cache to v73

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'beer-top100-v20260628-candlestick-3d-glow-v72';
+const CACHE_NAME = 'beer-top100-v20260628-candlestick-3d-glow-v73';
 const APP_SHELL = [
   './',
   './index.html',
