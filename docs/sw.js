8fc4c8322c1a703d5e23358ff0159c0b328bd217
Patiphan <patiphan.injob@gmail.com>
2026-07-04 22:18:24 +0700

    v3.9.22: Fix selected candle connector lines, watchlist fallback reset, and sync versions

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'beer-top100-v20260628-candlestick-3d-glow-v90';
+const CACHE_NAME = 'beer-top100-v20260628-candlestick-3d-glow-v91';
 const APP_SHELL = [
   './',
   './index.html',
