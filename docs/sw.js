f4bbaa8226b88e591e9ae1d44c83395ec3ecb9ef
Patiphan <patiphan.injob@gmail.com>
2026-07-04 22:01:00 +0700

    Fix universal candle clicking and market index journal navigation (v3.9.21)

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'beer-top100-v20260628-candlestick-3d-glow-v89';
+const CACHE_NAME = 'beer-top100-v20260628-candlestick-3d-glow-v90';
 const APP_SHELL = [
   './',
   './index.html',
--- docs/thai/sw.js
+++ docs/thai/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'thai-top100-v20260618-10';
+const CACHE_NAME = 'thai-top100-v20260618-11';
 const APP_SHELL = [
   './',
   './index.html',
