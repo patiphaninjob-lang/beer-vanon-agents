9790d6937d6ccf3dfcd588fa9a98b58a0ce93797
Patiphan <patiphan.injob@gmail.com>
2026-07-04 22:40:30 +0700

    v3.9.24: Remove History button from US homepage cards - users can click chart directly to expand

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'beer-top100-v20260628-candlestick-3d-glow-v92';
+const CACHE_NAME = 'beer-top100-v20260628-candlestick-3d-glow-v93';
 const APP_SHELL = [
   './',
   './index.html',
