bd63e5f730d0d57c3f0dd617c115317ae4919f96
Patiphan <patiphan.injob@gmail.com>
2026-07-04 17:49:30 +0700

    Fix event listener conflict on card mini charts (v3.9.10)

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'beer-top100-v20260628-candlestick-3d-glow-v79';
+const CACHE_NAME = 'beer-top100-v20260628-candlestick-3d-glow-v80';
 const APP_SHELL = [
   './',
   './index.html',
