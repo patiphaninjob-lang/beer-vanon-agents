7deba5927608b168131fcd625b66bf290a89136e
Patiphan <patiphan.injob@gmail.com>
2026-07-04 22:45:27 +0700

    v3.9.25: Remove market sentiment button from overview header - users can record via chart directly

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'beer-top100-v20260628-candlestick-3d-glow-v93';
+const CACHE_NAME = 'beer-top100-v20260628-candlestick-3d-glow-v94';
 const APP_SHELL = [
   './',
   './index.html',
