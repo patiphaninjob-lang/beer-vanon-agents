9f0080e4fe83105d75925b6f5c4536ea11a9e670
Patiphan <patiphan.injob@gmail.com>
2026-07-04 21:45:58 +0700

    Support both Ch34 checklists/analyses and market overview drivers on candle click in Journal and History pages (v3.9.18)

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'beer-top100-v20260628-candlestick-3d-glow-v87';
+const CACHE_NAME = 'beer-top100-v20260628-candlestick-3d-glow-v88';
 const APP_SHELL = [
   './',
   './index.html',
