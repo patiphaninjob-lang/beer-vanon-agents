9ae3d171de72edf089489a146e04b2c85ad10365
Patiphan <patiphan.injob@gmail.com>
2026-07-05 14:44:45 +0700

    feat: real-time notes tooltip sync between home and journal pages (v3.9.42)

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'beer-top100-v20260705-seasons-alert-v114';
+const CACHE_NAME = 'beer-top100-v20260705-realtime-tooltip-sync-v115';
 const APP_SHELL = [
   './',
   './index.html',
