4a26d8566cb5d0e32a66133574aed2db9facc039
Patiphan <patiphan.injob@gmail.com>
2026-07-05 10:31:59 +0700

    v3.9.31: Fully enable pointer-events on version tag and implement forceUpdateApp function to purge PWA cache properly

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'beer-top100-v20260705-mobile-save-ux-v101-reverted';
+const CACHE_NAME = 'beer-top100-v20260705-mobile-save-ux-v102-reverted';
 const APP_SHELL = [
   './',
   './index.html',
