093053b38dac9a031f25087d0e5f0ed09bc17c8d
Patiphan <patiphan.injob@gmail.com>
2026-07-05 14:36:13 +0700

    feat: integrate 6 market seasons alert on homepage header (v3.9.41)

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'beer-top100-v20260705-mobile-save-ux-v113-stats-bypassed';
+const CACHE_NAME = 'beer-top100-v20260705-seasons-alert-v114';
 const APP_SHELL = [
   './',
   './index.html',
